1a20660b9b1cb9f96def346e652fbc1dda07624b
mxdblcf <mxdblcf@163.com>
2021-09-02 21:09:49 +0800

    go study

--- leetcode/editor/cn/[217]存在重复元素.go
+++ leetcode/editor/cn/[217]存在重复元素.go
@@ -50,15 +50,6 @@ func containsDuplicate(nums []int) bool {
 	return false
 }
 
-func containsDuplicate1(nums []int) bool {
-	set := map[int]struct{}{}
-	for _, v := range nums {
-		if _, has := set[v]; has {
-			return true
-		}
-		set[v] = struct{}{}
-	}
-	return false
-}
+
 
 //leetcode submit region end(Prohibit modification and deletion)
