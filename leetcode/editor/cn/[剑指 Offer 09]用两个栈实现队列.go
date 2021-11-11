8a29c5cfe3cce4135b801f2be38658f0bc7e9f0c
mxdblcf <mxdblcf@163.com>
2021-11-11 16:57:05 +0800

    leetcode 141

---
+++ leetcode/editor/cn/[剑指 Offer 09]用两个栈实现队列.go
@@ -0,0 +1,58 @@
+//用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的
+//功能。(若队列中没有元素，deleteHead 操作返回 -1 ) 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：
+//["CQueue","appendTail","deleteHead","deleteHead"]
+//[[],[3],[],[]]
+//输出：[null,null,3,-1]
+// 
+//
+// 示例 2： 
+//
+// 输入：
+//["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+//[[],[],[5],[2],[],[]]
+//输出：[null,-1,null,null,5,2]
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= values <= 10000 
+// 最多会对 appendTail、deleteHead 进行 10000 次调用 
+// 
+// Related Topics 栈 设计 队列 👍 335 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+type CQueue struct {
+
+}
+
+
+func Constructor() CQueue {
+
+}
+
+
+func (this *CQueue) AppendTail(value int)  {
+
+}
+
+
+func (this *CQueue) DeleteHead() int {
+
+}
+
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AppendTail(value);
+ * param_2 := obj.DeleteHead();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
